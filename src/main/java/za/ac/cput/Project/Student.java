377d2e995d1c6c2943eed5e3c83bd177bc51932c
Muso Nkuntsu <231223722@mycput.ac.za>
2026-02-16 00:28:19 +0200

    base class code given

--- src/main/java/za/ac/cput/Project/Student.java
+++ src/main/java/za/ac/cput/Project/Student.java
@@ -1,4 +1,13 @@
 package za.ac.cput.Project;
 
-public class Student {
+public  abstract class Student {
+    protected String studentID;
+    protected String name;
+    protected String email;
+    protected String department;
+
+    public abstract double calculateTuition();
+    public abstract String getStudentType();
+
+    public void displayStudentDetails(){}
 }
